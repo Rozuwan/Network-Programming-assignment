dd7f9c1afd4666775f8904df82f149e50e6a0801
Roshan <pahariroshan55@gmail.com>
2026-09-23 09:21:20 +0545

    added and corrected Q13

--- Chapter_2_LAB_InetAddress/Question13.java
+++ Chapter_2_LAB_InetAddress/Question13.java
@@ -1,26 +1,41 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
 public class Question13 {
+
     public static void main(String[] args) {
+
         try (
             FileInputStream fin = new FileInputStream(args[0]);
             Reader in = new InputStreamReader(fin);
             BufferedReader bin = new BufferedReader(in)
         ) {
-            for (String entry = bin.readLine(); entry != null; entry = bin.readLine()) {
+
+            for (String entry = bin.readLine();
+                 entry != null;
+                 entry = bin.readLine()) {
+
                 int index = entry.indexOf(' ');
                 String ip = entry.substring(0, index);
                 String theRest = entry.substring(index);
 
                 try {
-                    InetAddress address = InetAddress.getByName(ip);
-                    System.out.println(address.getHostName() + theRest);
+
+                    InetAddress address =
+                        InetAddress.getByName(ip);
+
+                    System.out.println(
+                        address.getHostName() + theRest
+                    );
+
                 } catch (UnknownHostException ex) {
+
                     System.err.println(entry);
                 }
             }
+
         } catch (IOException ex) {
+
             System.out.println("Exception: " + ex);
         }
     }
